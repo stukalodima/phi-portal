15bc61cd329bad9552e70b0cc6adf49446338ffd
Dima <dima.stukalo@gmail.com>
2020-12-31 14:39:32 +0200

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace phi_portal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "ТОВАРИСТВА З ОБМЕЖЕНОЮ ВІДПОВІДАЛЬНІСТЮ \"ПРОДХІМІНДУСТРІЯ\"";
+
+            return View();
+        }
+
+        public ActionResult ZvitAudit()
+        {
+            return View();
+        }
+
+        public ActionResult ZvitUpr()
+        {
+            return View();
+        }
+
+        public ActionResult FinZvit()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="phi_portal.MvcApplication" Language="C#" %>
